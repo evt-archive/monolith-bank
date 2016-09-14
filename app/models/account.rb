56a3dfcd4c15063498dab6a36ac6d8044d7451b0
Aaron Dufall <aald212@gmail.com>
2016-09-13 21:48:53 -0700

    Add papertail to Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,3 @@
 class Account < ActiveRecord::Base
+  has_paper_trail
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914032604) do
+ActiveRecord::Schema.define(version: 20160914044719) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,16 @@ ActiveRecord::Schema.define(version: 20160914032604) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
